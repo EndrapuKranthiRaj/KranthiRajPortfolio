bbdb02b1dfc940f60c9ce13d42d0cfe56c1780cc
Kranthi <116026056+EndrapuKranthiRaj@users.noreply.github.com>
2024-02-21 19:32:55 +0530

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,16 +6,16 @@ export default function Footer() {
     const year = d.getFullYear();
     
   return (
-    <footer className={`bg-white rounded-sm shadow dark:bg-gray-800 relative ${window.location.pathname=="/admin"||"/" ? '': 'mt-16'}`}>
+    <footer className={`rounded-sm shadow bg-gray-800 relative ${window.location.pathname=="/admin"||"/" ? '': 'mt-16'}`}>
   <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+    <span className="text-sm  sm:text-center text-gray-400">
       © {year+" "}
       <Link  to="#" className="hover:underline">
         Portfolio™
       </Link >
       . All Rights Reserved.
     </span>
-    <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+    <ul className="flex flex-wrap items-center mt-3 text-sm font-medium  text-gray-400 sm:mt-0">
       <li>
         <Link  to="/about" className="hover:underline me-4 md:me-6">
           About
