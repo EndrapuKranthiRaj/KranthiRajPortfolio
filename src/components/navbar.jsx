2d8de0644d90cd826a9deda67137f3eadb5b936c
Kranthi <116026056+EndrapuKranthiRaj@users.noreply.github.com>
2024-02-21 19:29:13 +0530

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -44,7 +44,7 @@ export default function Navbar() {
           </svg>
         </button>
         <div className={`md:block w-full  md:w-auto ${isOpen ? 'block' : 'hidden'}`} id="navbar-default">
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border  rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white bg-gray-800 md:bg-gray-900 border-gray-700">
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border  rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 bg-gray-800 md:bg-gray-900 border-gray-700">
             <li>
               <Link
                 onClick={toggleMenu}
